b0767af1222e35321128d5896448e2b0dd45596a
mschodin <michael-schodin@uiowa.edu>
2020-06-03 11:58:11 -0500

    Commiting sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText} from '@trendmicro/react-sidenav';
+// import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import './side-nav.css';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SideNav
+    onSelect={(selected) => {
+        // Add your code here
+        if(selected === 'home'){
+            
+        }
+        else if (selected === 'charts'){
+
+        }
+    }}
+>
+    <SideNav.Toggle />
+    <SideNav.Nav defaultSelected="home">
+        <NavItem eventKey="home">
+            <NavIcon>
+                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Home
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="charts">
+            <NavIcon>
+                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Charts
+            </NavText>
+        </NavItem>
+    </SideNav.Nav>
+</SideNav>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Home extends React.Component {
+    render(){
+        return(
+            <h1>You are home</h1>
+        );
+    }
+}
+
+export default Home;
